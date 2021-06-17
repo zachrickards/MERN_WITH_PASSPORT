f90edf5f6f541d3bd6550eaa433cd228f1662bff
Anisha McDowell <anishamcdowell@gmail.com>
2021-06-17 19:44:55 -0400

    Added messages to routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const chatRoutes = require('./chats');
+const messageRoutes = require('./messages');
 
 router.use('/users', userRoutes);
-router.use('/chats', chatRoutes)
+router.use('/chats', chatRoutes);
+router.use('/messages', messageRoutes);
 
 module.exports = router;
