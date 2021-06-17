c4fb01cad1be18f5697a8fc8f06b61d029610d45
Anisha McDowell <anishamcdowell@gmail.com>
2021-06-17 19:37:17 -0400

    Added message model

---
+++ database/models/Message.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const Schema = mongoose.Schema;
+mongoose.promise = Promise;
+
+// Define messageSchema
+const messageSchema = new Schema({
+  chatId: { type: String },
+  sender: { type: String},
+  receiver: { type: String },
+  body: { type: String },
+  { timestamps: true }
+});
+
+// Define schema methods
+messageSchema.methods = {
+  checkPassword: function (inputPassword) {
+    return bcrypt.compareSync(inputPassword, this.password);
+  },
+  hashPassword: (plainTextPassword) => {
+    return bcrypt.hashSync(plainTextPassword, 10);
+  },
+};
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = message;
