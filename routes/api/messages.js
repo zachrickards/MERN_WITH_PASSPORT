f1f31b2a73186d5210440d83c49db923e669772c
Anisha McDowell <anishamcdowell@gmail.com>
2021-06-17 19:58:12 -0400

    Messages route working. Still using fake data from randomusers API; does not show message formatted data

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -12,14 +12,13 @@ const isAuth = (req, res, next) => {
 
 const Message = require("../../database/models/Message");
 const passport = require("../../passport");
-// TODO: CREATE MESSAGES CONTROLLER 
-// const messageController = require("../../controllers/chats");
+const messagesController = require("../../controllers/messages");
 
 //TODO: Display all messages to a single user (???MOVE THIS AS A METHOD TO CONTROLLERS ???)
 // router.get("/chatId/messageId", isAuth,  (req, res) => {
 //     res.send("Here is your Chats list!")
 // })
 
-// router.get("/chatId/messageId", messagesController.getFakeMessages);
+router.get("/chatId/messageId", messagesController.getFakeMessages);
 
 module.exports = router;
