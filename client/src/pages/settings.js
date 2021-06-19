2c89cdca7a8f06c67f62cc370663b3730fdb9c81
J_Kiser <jdkiser228@gmail.com>
2021-06-19 13:30:45 -0400

    adding profile/settings page

---
+++ client/src/pages/settings.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { LOADING, SET_USER } from '../store/actions';
+import { useStoreContext } from '../store/store';
+
+const Settings = () => {
+
+
+  return (
+    <div className="text-center">
+     {/* Delete profile
+     User profile pics - yellow
+    Link component - red
+    Hamburger menu/drop down menu - purple
+    Dot sliders - dark gray
+    Forms - blue refer to Google Slides
+ */}
+    </div>
+  );
+};
+
+export default Settings;
