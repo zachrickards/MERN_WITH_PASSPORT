8d93d194a2847e9be85ab0ef2fe1e716ce59bcb4
J_Kiser <jdkiser228@gmail.com>
2021-06-19 13:30:45 -0400

    adding profile/settings page

---
+++ client/src/pages/profile.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { LOADING, SET_USER } from '../store/actions';
+import { useStoreContext } from '../store/store';
+
+const Profile = () => {
+
+
+  return (
+    <div className="text-center">
+     {/* User card - turquoise is small cards to display all, 
+     light blue is individual user profile card
+        Nav/dash component - orange
+
+ */}
+    </div>
+  );
+};
+
+export default Profile;
