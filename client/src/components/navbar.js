45e511f088e84f8e85446183b0ea725a0214e21a
Anisha McDowell <anishamcdowell@gmail.com>
2021-06-19 17:52:03 -0400

    Basic navbar routes and hamburger menu functional! :)

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -28,49 +28,61 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to="/" className="btn btn-link text-secondary">
-        <span className="text-secondary">Home</span>
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
+      <div className="container-fluid">
+        <Link to="/" className="btn btn-link text-secondary">
+          <span className="text-secondary">Home</span>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           {state.user ? (
-            <li className="nav-item active">
-              <Link
-                to="#"
-                className="btn btn-link text-secondary"
-                onClick={logout}
-              >
-                <span className="text-secondary">Logout</span>
-              </Link>
-              <Link to="/settings" className="btn btn-link">
-                <span className="text-secondary">Settings</span>
-              </Link>
-              <Link to="/searchusers" className="btn btn-link">
-                <span className="text-secondary">Search Users</span>
-              </Link>
-              <Link to="/swipe" className="btn btn-link">
-                <span className="text-secondary">Find A Match</span>
-              </Link>
-              <Link to="/inbox" className="btn btn-link">
-                <span className="text-secondary">Messages</span>
-              </Link>
-              <Link to="/help" className="btn btn-link">
-                <span className="text-secondary">Help</span>
-              </Link>
-            </li>
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link
+                  to="#"
+                  className="btn btn-link text-secondary"
+                  onClick={logout}
+                >
+                  <span className="text-secondary">Logout</span>
+                </Link>
+              </li>
+              <li className="nav-item active">
+                <Link to="/settings" className="btn btn-link">
+                  <span className="text-secondary">Settings</span>
+                </Link>
+              </li>
+              <li className="nav-item active">
+                <Link to="/searchusers" className="btn btn-link">
+                  <span className="text-secondary">Search Users</span>
+                </Link>
+              </li>
+              <li className="nav-item active">
+                <Link to="/swipe" className="btn btn-link">
+                  <span className="text-secondary">Find A Match</span>
+                </Link>
+              </li>
+              <li className="nav-item active">
+                <Link to="/inbox" className="btn btn-link">
+                  <span className="text-secondary">Messages</span>
+                </Link>
+              </li>
+              <li className="nav-item active">
+                <Link to="/help" className="btn btn-link">
+                  <span className="text-secondary">Help</span>
+                </Link>
+              </li>
+            </ul>
           ) : (
-            <>
+            <ul className="navbar-nav">
               <li className="nav-item active">
                 <Link to="/login" className="btn btn-link text-secondary">
                   <span className="text-secondary">Login</span>
@@ -80,15 +92,77 @@ const Navbar = () => {
                 <Link to="/signup" className="btn btn-link">
                   <span className="text-secondary">Sign up</span>
                 </Link>
+              </li>
+              <li className="nav-item active">
                 <Link to="/help" className="btn btn-link">
                   <span className="text-secondary">Help</span>
                 </Link>
               </li>
-            </>
+            </ul>
           )}
-        </ul>
+        </div>
       </div>
     </nav>
+    // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    //   <Link to="/" className="btn btn-link text-secondary">
+    //     <span className="text-secondary">Home</span>
+    //   </Link>
+    //   <button
+    //     className="navbar-toggler"
+    //     type="button"
+    //     data-toggle="collapse"
+    //     data-target="#navbarNav"
+    //     aria-controls="navbarNav"
+    //     aria-expanded="false"
+    //     aria-label="Toggle navigation"
+    //   >
+    //     <span className="navbar-toggler-icon"></span>
+    //   </button>
+    //   <div className="collapse navbar-collapse" id="navbarNav">
+    //     <ul className="navbar-nav">
+    //       {state.user ? (
+    //         <li className="nav-item active">
+    //           <Link
+    //             to="#"
+    //             className="btn btn-link text-secondary"
+    //             onClick={logout}
+    //           >
+    //             <span className="text-secondary">Logout</span>
+    //           </Link>
+    //           <Link to="/settings" className="btn btn-link">
+    //             <span className="text-secondary">Settings</span>
+    //           </Link>
+    //           <Link to="/searchusers" className="btn btn-link">
+    //             <span className="text-secondary">Search Users</span>
+    //           </Link>
+    //           <Link to="/swipe" className="btn btn-link">
+    //             <span className="text-secondary">Find A Match</span>
+    //           </Link>
+    //           <Link to="/inbox" className="btn btn-link">
+    //             <span className="text-secondary">Messages</span>
+    //           </Link>
+    //           <Link to="/help" className="btn btn-link">
+    //             <span className="text-secondary">Help</span>
+    //           </Link>
+    //         </li>
+    //       ) : (
+    //         <>
+    //           <li className="nav-item active">
+    //             <Link to="/login" className="btn btn-link text-secondary">
+    //               <span className="text-secondary">Login</span>
+    //             </Link>
+    //             <Link to="/signup" className="btn btn-link">
+    //               <span className="text-secondary">Sign up</span>
+    //             </Link>
+    //             <Link to="/help" className="btn btn-link">
+    //               <span className="text-secondary">Help</span>
+    //             </Link>
+    //           </li>
+    //         </>
+    //       )}
+    //     </ul>
+    //   </div>
+    // </nav>
   );
 };
 
