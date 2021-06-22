888e2e6ac45be559b2837874f327ee0350cb5f37
Anisha McDowell <anishamcdowell@gmail.com>
2021-06-21 20:27:02 -0400

    Tinder card component rendering but not functional; will resolve later

--- client/src/App.js
+++ client/src/App.js
@@ -18,12 +18,11 @@ import { useStoreContext } from "./store/store";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faHeart } from '@fortawesome/free-solid-svg-icons'
+import clientSocket from "socket.io-client";
 
 library.add(fab, faEnvelope, faHeart)
 //Use throughout app where icons are needed: import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' with <FontAwesomeIcon icon="{iconName}" />
 
-
-import clientSocket from "socket.io-client";
 
 const PORT = "http://127.0.0.1:3001" //might need to change this to process.env.PORT to deploy on heroku
 const App = () => {
