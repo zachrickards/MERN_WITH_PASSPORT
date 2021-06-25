d6d4f1f2e13e82e8bf883e90083ad52f4c364314
Anisha McDowell <anishamcdowell@gmail.com>
2021-06-25 11:52:33 -0400

    Restored initial toggle state on edit/save icons; will attempt to lift state to editableContent component rather than pass around context

--- client/src/components/editIcon/editIcon.js
+++ client/src/components/editIcon/editIcon.js
@@ -1,57 +1,28 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./style.css";
-import EditContext from "../../contexts/editData/editContext";
 
-const EditIcon = (props) => {
-  //By default no content is editable yet
-  const [iconChange, setIconChange] = useState(false);
+const EditIcon = () => {
+  const [toggle, setToggle] = useState(false);
   const [icon, setIcon] = useState("edit");
   const [color, setColor] = useState("blue");
 
-//   //Toggle edit state to true on first click, then toggle normally w/ each click
-  const changeIcon = ({ icon, color }) => {
-    if (!iconChange) {
-      setIconChange(true);
+  const change = () => {
+    if (!toggle) {
+      setToggle(true)
       setIcon("save");
-      setColor("green");
-    } else {
-      setIconChange(false);
+      setColor("green")
+    } else if (toggle) {
+      setToggle(false)
       setIcon("edit");
       setColor("blue");
     }
   };
 
-  //TODO:
-  //RERENDER/POST NEW DATA TO DB ON SAVE
-
   return (
-    // <EditContext.Consumer>
-    //   {(context) => {
-    //     const { canEdit, canSave } = context;
-
-    //     const toggle = () => {
-    //         if (canEdit) {
-    //           canEdit.editContent();
-    //         } else if (canSave) {
-    //           canSave.saveContent();
-    //         }
-    //     };
-
-    //     return (
-          <button className="btn" 
-          onClick={changeIcon}
-          >
-            <FontAwesomeIcon
-              icon={icon}
-              size="xs"
-              color={color}
-              className="mx-2"
-            />
-          </button>
-    //     );
-    //   }}
-    // </EditContext.Consumer>
+    <button className="btn" onClick={change}>
+      <FontAwesomeIcon icon={icon} size="xs" color={color}className="mx-2" />
+    </button>
   );
 };
 
--- client/src/components/editableContent.js
+++ client/src/components/editableContent.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import EditIcon from "./editIcon/editIcon";
-import EditContext from "../contexts/editData/editContext";
+import InputGroup from "react-bootstrap/InputGroup";
 
 const EditableContent = (props) => {
-  // return (
-  //   <EditContext.Consumer>
-  //     {(context) => {
-        return (
-          <div 
-        //   className={}
-          >
-            {props.children}
-            <EditIcon />
-          </div>
-        );
-  //     }}
-  //   </EditContext.Consumer>
-  // );
+
+  const [canSave, setCanSave] = useState(true);
+  const [icon, setIcon] = useState("save");
+  const [color, setColor] = useState("green");
+  const [saveClass, setSaveClass] = useState("input-group-text");
+
+  const changeState = () => {
+    setCanSave(canSave ? false : canSave);
+    setIcon(icon === "edit" ? "save" : "edit");
+    setColor(color === "blue" ? "green" : "blue");
+    setSaveClass(
+      saveClass === "d-flex flex-row align-items-top justify-content-center"
+        ? "input-group-text"
+        : "d-flex flex-row align-items-top justify-content-center"
+    );
+  };
+
+  return (
+    <div className="">
+      {props.children}
+      <EditIcon />
+    </div>
+  );
 };
 
 export default EditableContent;
