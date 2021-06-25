142b803ed6f067477accf58947e02a8384f650f3
Anisha McDowell <anishamcdowell@gmail.com>
2021-06-25 16:47:26 -0400

    Working on reinstating editing context

--- client/src/contexts/editData/editContext.js
+++ client/src/contexts/editData/editContext.js
@@ -1,33 +1,21 @@
 import React, { createContext, useState } from "react";
 
-const EditContext = React.createContext();
+const EditContext = createContext();
 
-export const EditContextProvider = (props) => {
-    //By default no content is editable yet and all icons are the blue 'edit' icon
-    const [canEdit, setCanEdit] = useState({
-        icon: 'edit',
-        color: 'blue',
-        readonly: 'd-flex flex-row align-items-top justify-content-center',
-        editContent: () => {
-            setCanEdit(true);
-            console.log("canEdit is", canEdit.editable);
+export const EditProvider = ({ children }) => {
+    const [canEdit, setCanEdit] = useState("canEdit");
+
+    const toggleCanEdit = () => {
+        if (canEdit === "canEdit") {
+            setCanEdit("canSave");
+        } else {
+            setCanEdit("canEdit");
         }
-    });
-
-    const [canSave, setCanSave] = useState({
-        icon: 'save',
-        color: 'green',
-        textarea: 'textarea',
-        saveContent: ()  => {
-            setCanSave(true);
-            console.log("canSave is", canSave.saveable)
-            //update request
-        }
-    });
-
+    }
+;
     return (
-        <EditContext.Provider value={{ canEdit, canSave }}>
-            {props.children}
+        <EditContext.Provider value={{ canEdit, toggleCanEdit}}>
+            {children}
         </EditContext.Provider>
     )
 };
