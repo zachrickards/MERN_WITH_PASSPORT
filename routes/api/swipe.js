26838d239e2457171cea65fc938f0d2e617b8332
Canita Worth <cworth2264@gmail.com>
2021-07-08 18:20:02 -0400

    updated swipe functionality

--- routes/api/swipe.js
+++ routes/api/swipe.js
@@ -1,34 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../database/models/User");
-const { matchUsers } = require("/match.js");
+// const { matchUsers } = require("/match.js");
 
 //getting home endpoint and finding all user data and displaying it by their id
 router.get("/", async (req, res) => {
   try {
-    const myUser = await User.find({ _id: { $eq: req.user._id } });
-
+    const myUser = await User.findOne({ _id: { $eq: req.user._id } });
+    // await myUser.execPopulate({ path: "yesSwipes", select: "User" });
     // TODO: filter users based on previous matches
     const userData = await User.find({
       _id: {
         $ne: req.user._id,
       },
     });
-
+    let test = userData.yesSwipes;
     console.log(
       "FILTERED USERS",
       userData.filter((user) => {
         // Check if user exists in my user yes data
-        return myUser.yes.findIndex((_id) => _id === user._id) === -1;
+        return myUser.yesSwipes?.findIndex((_id) => _id === user._id) === -1;
       })
     );
     //turning user data to JSON
-    res.json(
-      userData.filter((user) => {
-        // Check if user exists in my user yes data
-        return myUser.yes.findIndex((_id) => _id === user._id) === -1;
-      })
-    );
+    res.json(userData);
   } catch (err) {
     console.log(err);
     res.json(err);
