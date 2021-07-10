9843c1b9f9941bfa552176833e21542f79211e57
J_Kiser <jdkiser228@gmail.com>
2021-07-10 13:23:52 -0400

    fixing homepage image/button

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,32 +1,27 @@
-import React from 'react';
-import './home.css';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./home.css";
+import { Button } from 'react-bootstrap'
 
 const Home = (props) => {
-  return( 
-  <div
-  className='hero-container'
-  style={{
-    width: "100%",
-    height: "100vh",
-    backgroundImage: `url('https://www.google.com/imgres?imgurl=https%3A%2F%2Fus.123rf.com%2F450wm%2Fgorgev%2Fgorgev1811%2Fgorgev181100728%2F112908422-attractive-beautiful-couple-is-holding-hands-outdoors-on-a-sunny-day.jpg%3Fver%3D6&imgrefurl=https%3A%2F%2Fwww.123rf.com%2Fstock-photo%2Fcouple_holding_hands.html&tbnid=mhXEQVtXuCS8JM&vet=10CIEBEDMooQFqFwoTCMjh9pnE1_ECFQAAAAAdAAAAABAC..i&docid=ldaPbrxgbCvL8M&w=450&h=300&q=couple%20holding%20hands%20real%20images&ved=0CIEBEDMooQFqFwoTCMjh9pnE1_ECFQAAAAAdAAAAABAC')`,
-    backgroundPosition: "fixed",
-    backgroundSize: "cover",
-  }}
->
-  <h1>Connect the Dots to True Love...</h1>
-  <p>What are you waiting for?</p>
-  <div className='home-btns'>
-    {/* <Button 
-      className='btns'
-      buttonStyle='btn--outline'
-      buttonSize='btn--large'
-      >
-      GET STARTED
-      </Button> */}
-  </div>
-  </div>
-  )
-}
+  const history = useHistory()
+  return (
+    <div
+      className="d-flex flex-column align-items-center justify-content-around"
+      style={{
+        width: "100%",
+        height: "100vh",
+        backgroundImage: `url('https://us.123rf.com/450wm/gorgev/gorgev1811/gorgev181100728/112908422-attractive-beautiful-couple-is-holding-hands-outdoors-on-a-sunny-day.jpg?ver=6')`,
+        backgroundPosition: "fixed",
+        backgroundSize: "cover",
+      }}
+    >
+      <h1>Connect the Dots to True Love...</h1>
+      <Button className="px-5 py-3" variant="danger" size="lg" onClick={() => history.push("/swipe")}>Get Started</Button>
+      <h2>What are you waiting for?</h2>
+    </div>
+  );
+};
 
 Home.propTypes = {};
 
