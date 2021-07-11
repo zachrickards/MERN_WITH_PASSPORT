4ca9f6697669f645070378a67d36ee865100b524
Anisha McDowell <anishamcdowell@gmail.com>
2021-07-11 16:09:35 -0400

    updated file paths

--- routes/api/image.js
+++ routes/api/image.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../../database/models/User");
+const User = require("../../database/models/user");
 const passport = require("../../passport");
 const upload = require('../../lib/multer');
 const cloudinary = require('../../lib/cloudinary');
